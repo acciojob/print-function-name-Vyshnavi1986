201314ad69291a603f02041b09b92bbecade004d
Vyshnavi1986 <157476467+Vyshnavi1986@users.noreply.github.com>
2024-04-28 11:41:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+ function GFGs() { 
+            var me = arguments.callee.toString(); 
+            me = me.substr('function '.length);      
+            me = me.substr(0, me.indexOf('('));      
+            alert(me); 
+        } 
+        GFGs();    
