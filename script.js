d3971b79473a5efcbee9ae396115a7d8745beaae
Vyshnavi1986 <157476467+Vyshnavi1986@users.noreply.github.com>
2024-04-28 11:44:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-//your JS code here. If required.
- function GFGs() { 
-            var me = arguments.callee.toString(); 
-            me = me.substr('function '.length);      
-            me = me.substr(0, me.indexOf('('));      
-            alert(me); 
-        } 
-        GFGs();    
+function getFuncName() {
+    return getFuncName.caller.name
+}
+function teddy() { 
+    console.log(getFuncName())
+}
+teddy()
